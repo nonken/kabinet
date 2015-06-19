a0c78d37a22a12e1e52bc018174e93a8cd9e4ad0
Matthijs van Henten <matthijs@c9.io>
2015-06-19 23:36:19 +0000

    alt implementation of connectostores

---
+++ store-connect.js
@@ -0,0 +1,62 @@
+"usee strict";
+
+var React = require('react');
+
+module.exports = function StoreConnect(def) {
+    if (!def.storeProps)
+        throw "You should now wrap a component without storeProps";
+
+    var name = def.displayName || "";
+
+    return React.createClass({
+
+        displayName: name + "StoreWrapper",
+
+        contextTypes: {
+            getStore: React.PropTypes.func.isRequired,
+        },
+
+        getInitialState: function() {
+            return this.getStateFromStores();
+        },
+
+        componentDidMount: function componentDidMount() {
+            this._getStores().forEach(function(kv) {
+                kv.store.observe(kv.attribute, this._onStoreChange);
+            }, this);
+        },
+
+        componentWillUnmount: function componentWillUnmount() {
+            this._getStores().forEach(function(kv) {
+                kv.store.stopObserving(kv.attribute, this._onStoreChange);
+            }, this);
+        },
+
+        _getStores: function(handle) {
+            return Object.keys(def.storeProps).map(function(key) {
+                return {
+                    store: this.context.getStore(def.storeProps[key].store),
+                    attribute: key,
+                };
+            }, this);
+        },
+
+        _onStoreChange: function onStoreChange() {
+            if (!this.isMounted())
+                return;
+
+            this.setState(this.getStateFromStores());
+        },
+
+        getStateFromStores: function() {
+            return this._getStores().reduce(function(state, kv) {
+                state[kv.attribute] = kv.store.state[kv.attribute];
+                return state;
+            }.bind(this), {});
+        },
+
+        render: function() {
+            return React.createElement( React.createClass(def), this.state );
+        }
+    });
+};
