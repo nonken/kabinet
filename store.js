221580f21fc382181151e2ebb9ff88272f8755cd
Nikolai Onken <nikolai@nikolaionken.com>
2017-06-03 17:39:16 +0200

    Refactor into class

--- store.js
+++ store.js
@@ -1,7 +1,8 @@
-"use strict";
+'use strict';
 
 const State = new WeakMap();
 const _ = require('lodash');
+import PropTypes from 'prop-types';
 
 const clone = (value) => {
     if (Array.isArray(value))
@@ -30,58 +31,51 @@ function observers() {
     state.observers.forEach(fn => fn.call(null, this.getState()));
 }
 
-const proto = {
-    getState() {
+export class Store {
+    constructor(state) {
+        State.set(this, {
+            name: this.constructor.name,
+            state: state || {},
+            observers: new Map()
+        });
+    }
+
+    getState = () => {
         return clone(State.get(this).state);
-    },
+    };
 
-    observe(fn, scope) {
+    observe = (fn, scope) => {
         State.get(this).observers.set(fn, fn.bind(scope));
-    },
+    };
 
-    stopObserving(fn) {
+    stopObserving = (fn) => {
         State.get(this).observers.delete(fn);
-    },
+    };
 
-    clearState() {
+    clearState = () => {
         State.get(this).state = {};
         observers.call(this);
-    },
+    };
 
-    setState(name, value) {
+    setState = (name, value) => {
+        const propTypes = this.constructor.propTypes;
+
         let state = State.get(this);
         let newState = name;
 
-        if (arguments.length == 2)
+        if (value)
             newState = {[name]: value};
 
+        PropTypes.checkPropTypes(this.constructor.propTypes, newState, 'store', state.name);
+
         for (let key in newState) {
-            if (!(key in state.storeProps))
+            if (!(key in propTypes)) {
                 throw new TypeError(`unknown property "${name}" for ${state.name}`);
-            check(key, state.storeProps[key], newState[key]);
+            }
+
             state.state[key] = clone(newState[key]);
         }
 
         observers.call(this);
-    }
-};
-
-module.exports.create = function create(name = "StoreName", storeProps = {}) {
-    check('first argument "name"', String, name);
-    check('second argument "storeProps"', Object, storeProps);
-
-    const ctor = function(state) {
-        State.set(this, {
-            name: name,
-            state: state || {},
-            observers: new Map(),
-            storeProps: storeProps
-        });
     };
-
-    ctor.prototype = Object.create(proto);
-
-    return Object.defineProperty(ctor, "name", {
-        value: name
-    });
-};
+}
--- store_test.js
+++ store_test.js
@@ -1,82 +1,88 @@
 "use strict";
 
-var test = require("tape");
-var sinon = require("sinon");
-var Store = require("./store");
+const test = require("tape");
+const sinon = require("sinon");
 
-test("Store.create", function(assert){
-    assert.equal(Store.create("TestStore").name, "TestStore", "It creates a named function");
+import { Store } from './store';
+import PropTypes from 'prop-types';
 
-    assert.throws(() => {
-        Store.create(1);
-    }, /type of first argument "name" must be String/);
-    
-    assert.throws(() => {
-        Store.create("test", 33);
-    }, /type of second argument "storeProps" must be Object/);
+test.skip("Store", function(assert){
+    class TestStore extends Store {}
+    const store = new TestStore();
+
+    assert.equal(typeof store.setState, 'function', 'setState should be a function');
+    assert.equal(typeof store.getState, 'function', 'getState should be a function');
+    assert.equal(typeof store.clearState, 'function', 'clearState should be a function');
+    assert.equal(typeof store.observe, 'function', 'observe should be a function');
+    assert.equal(typeof store.stopObserving, 'function', 'stopObserving should be a function');
 
     assert.end();
 });
 
 test("Store.setState", function(assert){
-    let MyStore = Store.create("MyStore", {
-        things: Array
-    });
-    
-    let store = new MyStore();
+    class TestStore extends Store {
+        static propTypes = {
+            things: PropTypes.array
+        };
+    }
+
+    const store = new TestStore();
+
     let things = [1,2,3];
 
     assert.deepEqual(store.getState(), {});
     store.setState("things", things);
 
     assert.deepEqual(store.getState(), { things: things }, "Thing was stored");
-    
+
     let copy = store.getState().things;
     copy.push(42);
 
     assert.deepEqual(store.getState(), { things: things }, "State was immutable");
-    
+
     let thingsCloned = [].concat(things);
     things.push(43);
 
     assert.deepEqual(store.getState(), { things: thingsCloned }, "State is not a ref to the original");
-    
-    assert.throws(() => {
-        store.setState("things", {});
-    }, /TypeError: type of things must be Array, not object/, "State is typechecked");
-    
+
+    let observer = sinon.spy(console, 'error');
+    store.setState("things", {});
+    assert.ok(observer.calledWith('Warning: Failed store type: Invalid store `things` of type `object` supplied to `TestStore`, expected `array`.'), 'Validates properties.');
+
     assert.throws(() => {
         store.setState("others", {});
-    }, /unknown property "others" for MyStore/, "Cannot add unknown properties");
-    
-    let alt = new MyStore();
-    
+    }, TypeError, "Cannot add unknown properties");
+
+    let alt = new TestStore();
+
     assert.notDeepEqual(alt.getState(), store.getState(), "Stores don't share state");
-    
+
     store.setState({ things: [] });
 
     assert.end();
 });
 
 test("Store.observe", function(assert){
-    let Test = Store.create("Test", {
-        count: Number
-    });
+    class TestStore extends Store {
+        static propTypes = {
+            count: PropTypes.number
+        };
+    }
 
-    let store = new Test();
-    
+    let store = new TestStore();
+
     let observer = sinon.spy();
-    
+
     store.observe(observer);
-    
+
     store.setState("count", 1);
-    
+
     assert.ok(observer.calledOnce, "Observer was called");
-    
+
     assert.ok(observer.calledWith({ count: 1 }));
-    
+
     store.setState("count", 2);
-    
+
     assert.ok(observer.calledWith({ count: 2 }));
 
     assert.ok(observer.calledTwice, "Observer called twice");
@@ -85,73 +91,79 @@ test("Store.observe", function(assert){
 });
 
 test("Store.stopObserving", function(assert){
-    let Test = Store.create("Test", {
-        count: Number
-    });
-    
-    let store = new Test();
+    class TestStore extends Store {
+        static propTypes = {
+            count: PropTypes.number
+        };
+    }
+
+    let store = new TestStore();
     let observer = sinon.spy();
-    
+
     store.observe(observer);
     store.setState("count", 1);
-    
+
     assert.ok(observer.calledOnce, "Observer was called");
-    
+
     store.stopObserving(observer);
     store.setState("count", 2);
     assert.ok(observer.calledOnce, "Observer was not called again");
-    
+
     store.stopObserving(observer);
     assert.pass("Calling stopObserving again did not crash");
-    
+
     assert.end();
 });
 
 test("Store.observers", function(assert){
-    let Test = Store.create("Test", {
-        count: Number
-    });
-    
-    let store = new Test();
+    class TestStore extends Store {
+        static propTypes = {
+            count: PropTypes.number
+        };
+    }
+
+    let store = new TestStore();
     let first = sinon.spy();
     let second = sinon.spy();
-    
+
     store.observe(first);
     store.observe(second);
-    
+
     store.setState("count", 2);
 
     assert.ok(first.calledOnce, "First observer was called");
     assert.ok(second.calledOnce, "Second observer was called");
-    
+
     store.stopObserving(second);
-    
+
     store.setState("count", 2);
-    
+
     assert.ok(first.calledTwice, "First observer called again");
     assert.ok(second.calledOnce, "Second observer was not called");
 
-    assert.end();    
+    assert.end();
 });
 
 test("Store.clearState", function(assert){
-    let Test = Store.create("Test", {
-        count: Number
-    });
-    
-    let store = new Test();
+    class TestStore extends Store {
+        static propTypes = {
+            count: PropTypes.number
+        };
+    }
+
+    let store = new TestStore();
     let observer = sinon.spy();
     let count = Math.random();
-    
+
     store.observe(observer);
     store.setState("count", count);
 
     assert.ok(observer.calledOnce, "Observer was called");
     assert.ok(observer.calledWith({ count: count }));
     assert.deepEqual(store.getState(), { count: count });
-    
+
     store.clearState();
-    
+
     assert.ok(observer.calledTwice, "Observer was called");
     assert.ok(observer.calledWith({}));
     assert.deepEqual(store.getState(), {});
